a31ecc98237ba22fdaf8b832922f0c3a9058ff65
Michael Schmidt <mschmidty@gmail.com>
2015-07-23 17:25:17 -0600

    changes hnome link section

--- home.php
+++ home.php
@@ -10,15 +10,15 @@ Template Name: Homepage
 	<img sizes="100vw" src = "<?php echo get_template_directory_uri(); ?>/images/home/teal-face-medium.jpg"
 srcset="<?php echo get_template_directory_uri(); ?>/images/home/teal-face-xsmall.jpg 500w, <?php echo get_template_directory_uri(); ?>/images/home/teal-face-small.jpg 700w, <?php echo get_template_directory_uri(); ?>/images/home/teal-face-medium.jpg 1000w, <?php echo get_template_directory_uri(); ?>/images/home/teal-face-large.jpg 1500w" alt="Teal looking at a score board">
 </div>
-<section class="width-100 home-links">
+<section class="home-links">
   <a href="/index.php/videos" class="home-links-child centered-verticle-container">
     <div class="centered-verticle">
       <h3 >Videos</h3>
     </div>
   </a>
-  <a href="/index.php/sponsors" class="home-links-child centered-verticle-container">
+  <a href="/index.php/schedule" class="home-links-child centered-verticle-container">
       <div class="centered-verticle">
-        <h3>Sponsors</h3>
+        <h3>Schedule</h3>
       </div>
   </a>  
   <a href="/index.php/about" class="home-links-child centered-verticle-container">
@@ -35,6 +35,9 @@ srcset="<?php echo get_template_directory_uri(); ?>/images/home/teal-face-xsmall
         src="<?php echo get_template_directory_uri(); ?>/images/home/teal-fontana-medium.jpg"
         srcset="<?php echo get_template_directory_uri(); ?>/images/home/teal-fontana-large.jpg 1500w, <?php echo get_template_directory_uri(); ?>/images/home/teal-fontana-medium.jpg 1000w, <?php echo get_template_directory_uri(); ?>/images/home/teal-fontana-small.jpg 700w, <?php echo get_template_directory_uri(); ?>/images/home/teal-fontana-xsmall.jpg 500w" >
   </div>
+</section>
+<section class="width-100 home-sponsers">
+  
 </section>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
